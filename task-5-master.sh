c7a8461488b684fa941022da67afc8e42bdb5dc2
Hamood Ahmed <hamoodahmed75a@gmail.com>
2026-03-30 16:38:17 +0500

    Update task-5-master.sh

--- task-5-master.sh
+++ task-5-master.sh
@@ -1,7 +1,7 @@
 # This shell script that calls the load generator, followed by task-05-pipeline. 
 # This inputs the data into a mongo db database   
 
-# usage: sh ./task-05-master.sh http://localhost:8080/primecheck 10 10 5 load-generator-number
+# usage: sh ./task-5-master.sh http://localhost:8080/primecheck 10 10 5 load-generator-number
 # returns: a csv file with database names for use in the r script, a csv file with collection names for use in the r script 
 
 #!/bin/bash
